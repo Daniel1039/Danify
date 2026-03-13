99422bfcf31a0f859f9440b3b36e463bcaca0e34
Daniel1039 <87763035+Daniel1039@users.noreply.github.com>
2026-03-13 04:53:26 -0700

    Update ALLOWED_HOSTS for deployment

--- cbt_project/settings.py
+++ cbt_project/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-me'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'danify-ea5k.onrender.com',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
